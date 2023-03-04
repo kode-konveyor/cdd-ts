290ae41454c17a1aaf52140f80fab1f143c44b8e
Arpad Magosanyi <m4gw4s@gmail.com>
2023-03-04 15:45:12 +0100

    async test made properly, typecast to make stryker happy, but it is not

--- src/runContractsfromList.ts
+++ src/runContractsfromList.ts
@@ -1,3 +1,5 @@
+import {BeThat} from "./BeThat"
+import { Check } from "./Check";
 
 export async function runContractsfromList(contracts: string[], dir: string) {
     let promises: Promise<number>[] = []
@@ -18,6 +20,7 @@ export async function runContractsfromList(contracts: string[], dir: string) {
 async function runOneContract(dir: string, contractFile: string, contractName: string) {
     const modulePromise = import(dir + "/" + contractFile);
     const module = await modulePromise;
-    return module[contractName].check() as number;
+    const contract: Check<any> = module[contractName];
+    return contract.check() as number;
 }
 
