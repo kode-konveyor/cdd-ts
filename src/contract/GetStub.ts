8ef5e92ceaef9fca6ddfa65ff366a12fb99017ee
Arpad Magosanyi <m4gw4s@gmail.com>
2023-03-25 14:40:26 +0100

    getStrub uses this

--- src/contract/GetStub.ts
+++ src/contract/GetStub.ts
@@ -7,17 +7,17 @@ import { getParametersFromGetters } from "../util/getParametersFromGetters.js";
 
 export class GetStub {
     getStub<T extends MethodType>(
-        contract: ContractEntity<T>,
+        this: ContractEntity<T>,
         caseName?: string
     ): T {
         if (caseName == null)
             caseName = ""
-        if (contract.currentRun != null) {
-            const currentCase = (contract.currentCase != null) ? contract.currentCase : "";
-            contract.cases[currentCase].runs.push(contract.currentRun)
+        if (this.currentRun != null) {
+            const currentCase = (this.currentCase != null) ? this.currentCase : "";
+            this.cases[currentCase].runs.push(this.currentRun)
         }
 
-        const currentCase = contract.cases[caseName];
+        const currentCase = this.cases[caseName];
 
         const stub = (...params: Parameters<T>): ReturnType<T> => {
             const retvals: Array<ReturnType<T>> = []
@@ -37,7 +37,7 @@ export class GetStub {
                         throw new Error(String(run.thrown))
             })
             if (retvals.length !== 1) {
-                console.log(contract)
+                console.log(this)
                 throw new Error(messageFormat(
                     MORE_RETURN_VALUES_FOR_ONE_PARAMETER_SET_MESSAGE_FORMAT,
                     params.toString(),
