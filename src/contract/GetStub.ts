124fb5dce4ba6b9d1d83ba0b45b426356e79fc39
Arpad Magosanyi <m4gw4s@gmail.com>
2023-04-01 13:21:32 +0200

    getStub cleanup

--- src/contract/GetStub.ts
+++ src/contract/GetStub.ts
@@ -6,6 +6,7 @@ import { MORE_RETURN_VALUES_FOR_ONE_PARAMETER_SET_MESSAGE_FORMAT } from "./Messa
 import { getParametersFromGetters } from "../util/getParametersFromGetters.js";
 import { CaseName } from "../check/CaseName.js";
 import { CheckCurrentRun } from "./CheckCurrentRun.js";
+import { serialize } from "../util/serialize.js";
 
 export class GetStub<T extends MethodType> extends ContractEntity<T> {
     constructor(
@@ -30,8 +31,6 @@ export class GetStub<T extends MethodType> extends ContractEntity<T> {
                 if (run.parameterGetters == null)
                     throw new Error(messageFormat(
                         "A run in the case '{1}' have no parameters defined!",
-                        // made sure that it is not null as the first thing. TS forgot that here
-                        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
                         String(caseName)))
                 if(run.parameterConstraints.length > 0) {
                     if(
@@ -51,7 +50,7 @@ export class GetStub<T extends MethodType> extends ContractEntity<T> {
             if (retvals.length !== 1) {
                 throw new Error(messageFormat(
                     MORE_RETURN_VALUES_FOR_ONE_PARAMETER_SET_MESSAGE_FORMAT,
-                    params.toString(),
+                    serialize(params),
                     retvals.length.toString()))
             }
             return retvals[0]()
